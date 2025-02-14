c8bcb9f6f33417a93cd33ad1c8f5925861e0e912
ScienceProject.io <162120681+Ivancaswda@users.noreply.github.com>
2025-02-14 12:36:12 +0200

    Update vite.config.js

--- administrator/vite.config.js
+++ administrator/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: {port: 5555}
+  port: {port: 5555},
+  build: {
+    rollupOptions: {
+      external: ['react-toastify', 'react-toastify/dist/ReactToastify.css']
+    }
+  }
 })
